5c96623f3936205cafec059e382402fde6d10b95
Troy Starwalt <troy.starwalt@gmail.com>
2017-08-04 13:46:02 -0400

    writes two specs for visiting home page

--- spec/features/pages/visit_root_spec.rb
+++ spec/features/pages/visit_root_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'Visiting root route' do
   let(:admin) { FactoryGirl.create(:admin, :confirmed) }
-  scenario 'as a guest you see nothing.' do
+  scenario 'As a visitor, you see navbar' do
     visit '/'
-
-    expect(page).not_to have_button('Sign Out')
+    expect(page).to have_link('who')
   end
+
+  scenario 'As a visitor, you see the opening header' do
+    visit '/'
+    expect(page).to have_selector('h1')
+  end
+
 end
